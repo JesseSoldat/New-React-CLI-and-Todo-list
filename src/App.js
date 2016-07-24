52fb6cb9b0ccf99b1fa04aac1305866dd130d6e5
jesse soldat <jsoldat@hotmail.com>
2016-07-24 08:43:25 +0700

    created todo component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import Todo from './Todo';
+
 import logo from './logo.svg';
 import './App.css';
 
@@ -10,9 +12,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h2>Welcome to React</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Todo></Todo>
       </div>
     );
   }
---
+++ src/Todo.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+
+
+class Todo extends Component {
+  render() {
+    return (
+      <div className="todo">
+        <h3>TODO APP</h3>
+      </div>
+    );
+  }
+}
+
+export default Todo;
